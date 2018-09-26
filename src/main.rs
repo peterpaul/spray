added38484c92d9bb7587c9fa96e1a8289682ec4
Peterpaul Taekele Klein Haneveld <pp.kleinhaneveld@gmail.com>
2018-09-26 08:25:52 +0200

    Store writers in map for efficiency reasons

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use regex::Regex;
 use std::io::{self, BufRead, BufWriter, Write};
 use std::env;
 use std::collections::HashMap;
-use std::fs::{File, OpenOptions};
+use std::fs::OpenOptions;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -13,19 +13,22 @@ fn main() {
     let reader = stdin.lock();
     let pattern = Regex::new(args[1].as_str()).unwrap();
     let replacement = args[2].as_str();
+    let mut files = HashMap::new();
     for line_result in reader.lines() {
         match line_result {
             Ok(line) => {
                 let filename = pattern.replace_all(line.as_str(), replacement).into_owned();
-                let handle = OpenOptions::new()
-                    .create(true)
-                    .write(true)
-                    .truncate(false)
-                    .append(true)
-                    .open(filename)
-                    .expect("Failed to open file");
-                let mut writer = BufWriter::new(handle);
-                writeln!(writer, "{}", line).unwrap();
+                let writer = files.entry(filename.clone())
+                    .or_insert_with(|| {
+                        BufWriter::new(OpenOptions::new()
+                            .create(true)
+                            .write(true)
+                            .truncate(false)
+                            .append(true)
+                            .open(filename.clone())
+                            .expect("Failed to open file"))
+                    });
+                writer.write_fmt(format_args!("{}", line)).unwrap();
             },
             Err(e) => {
                 println!("error reading line: {:?}", e);
